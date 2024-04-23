990578e25d3121d80c3598b1f0995497688b7825
João Bittencourt <joao@DESKTOP-U30RQRA>
2024-04-23 20:38:48 -0300

    adicionando exemplos de como usar paramentros

--- 01-functions.js
+++ 01-functions.js
@@ -7,6 +7,19 @@ const minhaArrowFunction = () => {
     this.name = 'João'
 };
 
-minhaArrowFunction()
+function usandoVariavelParametros() {
+    console.log(arguments)
+    //A variavel arguments retornar um objeto com os parametros passados na chamada da função, não importa quantos eles são
+}
 
-console.log(this)
+usandoVariavelParametros(1, 'João', true)
+
+//Há uma outra forma de fazermos isso, através do rest operator:
+
+function usandoRestOperator(parametro1, ...restanteParametros) {
+    console.log({parametro1})
+    console.log({...restanteParametros})
+    //Desta forma podemos até escolher
+}
+
+usandoRestOperator(0, 1, 2, 3, 4, 5)
