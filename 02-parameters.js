506b8a3642ed6235da3f805e3a7779a60d46a5ac
João Bittencourt <joao@DESKTOP-U30RQRA>
2024-04-24 18:43:25 -0300

    como adicionar valores padrao em um parametro

---
+++ 02-parameters.js
@@ -0,0 +1,6 @@
+function greetings (mensagem = 'Mensagem Padrão') {
+    //Essa é uma forma simples de retornarmos um valor default para um parâmentro (caso na chamada da função não seja passado nenhum.)
+    console.log(mensagem)
+}
+
+greetings()
